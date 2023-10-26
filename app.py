7a46980b3842444e86dd11ae4bdc579e91b1d683
loremxipsum <149025169+loremxipsum@users.noreply.github.com>
2023-10-25 20:30:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,34 @@
 import streamlit as st
 
-st.title('PILOT
+st.title('PILOT')
 
-
+st.write('''
 HELLO TEAM
-We are currently a simple group of Technologists. We do the mapping – we do the supply chain. We use cutting edge AI in pursuit of our R&D that aims to push the frontiers of how we percieve technology. We are only devising supply chain and logistics solutions here at this point in time. But, we are here to use the same framework that goes into building these solutions to change intelligence as we know it – forever. 
 
-We will soon build supply chain and logistics systems robotized, governed and robustized to the point where we will be capable of transporting anything from hash-tokenized-military-assets to sugar. But for now, we do salt. In the re-interpretized version of Gandhi, we do salt – we do salt from the international waters through borders. 
+We are currently a simple group of Technologists. We do the mapping – we do the supply chain. We use cutting edge AI in pursuit of our R&D that aims to push the frontiers of how we perceive intelligent-technology. We are only devising supply chain and logistics solutions here at this point in time. But, we are here to use the same framework that goes into building these solutions to change intelligence as we know it – forever.
 
-We start with the operational expenses we have access to from small tier logistics companies with a fleet of 500 trucks that comply with standards set forth by ______ (allowing us to architecture governance easily around the vendors and other parties involved). 
+We will soon build supply chain and logistics systems robotized, governed, and robustized to the point where we will be capable of transporting anything from hash-tokenized-military-assets to sugar. But for now, we do salt. In the re-interpretized version of Gandhi, we do salt – we do salt from the international waters through borders.
 
-Our vectorized smart contract calculates your contributions and logs them into the blockchain automating your immediate transfer of funds on your centralized-by-design, decentralized-in-nature metamask wallet on a 4-chain confirmation basis. Contact me at: admin@loremipsumscl.com for issues with your transfers. We are currently in the process of building a UI that allow you to view tokens you have earned.  Until then contact admin. 
+We start with the operational expenses we have access to from small tier logistics companies with a fleet of 500 trucks that comply with standards set forth by ______ (allowing us to architecture governance easily around the vendors and other parties involved).
 
-Further content can be found via accesing this content on a tor browser: 
+Our vectorized smart contract calculates your contributions and logs them into the blockchain automating your immediate transfer of funds on your centralized-by-design, decentralized-in-nature metamask wallet on a 4-chain confirmation basis. Contact me at: admin@loremipsumscl.com for issues with your transfers. We are currently in the process of building a UI that allows you to view tokens you have earned. Until then contact admin.
+
+Further content can be found via accessing this content on a tor browser:
 
 loremipsumscl.onion
 
-contact admin for tor access guide – tor access guide will only be provided to you based on your answer to how you will contribute to the current deliverables. See you on day 33.
+Contact admin for tor access guide – tor access guide will only be provided to you based on your answer to how you will contribute to the current deliverables. See you on day 33.
 
 ///
 ----Tor----
-For the first deliverables, we would require a clone of craigslist. The world is a massive downtown of night creatures who’re looking to push the frontiers of what is currently being perceived as intelligence – need I remind you again – the foundation to what we will architecture is currently the foundation to a revolutionary supply-chain management system for transportation of life-essential-salt across one of the most politically conflicted regions in the world ++++ but what it will become of is something that will disrupt the system and break the doors to our perception of intelligence. Now, grab on your commodores, back-door the firmware as we pop nodes around the globe for what will start as a project that open-sources a clone of craigslist... 
+
+For the first deliverables, we would require a clone of craigslist. The world is a massive downtown of night creatures who’re looking to push the frontiers of what is currently being perceived as intelligence – need I remind you again – the foundation to what we will architecture is currently the foundation to a revolutionary supply-chain management system for transportation of life-essential-salt across one of the most politically conflicted regions in the world ++++ but what it will become of is something that will disrupt the system and break the doors to our perception of intelligence. Now, grab on your commodores, back-door the firmware as we pop nodes around the globe for what will start as a project that open-sources a clone of craigslist...
 
 [keep checking this page every 33 days for updates]
-\\
-== your references for now: www.craigslist.org 
-Check back on day 33 for updated contributions on the chain. 
-This is a private chain that is made public on reasonable contribution – this for reduced spamming on our repos. Please commit to coding standards. If she uses space bar for tabs...
-un-date herr
 
 \\
-‘\
 
-
-')
+== Your references for now: www.craigslist.org
+Check back on day 33 for updated contributions on the chain.
+This is a private chain that is made public on reasonable contribution – this for reduced spamming on our project.
+''')
