c816ec8c9c91f2546dcf9131e6a5202347074eaa
loremxipsum <149025169+loremxipsum@users.noreply.github.com>
2023-10-25 22:07:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
+
+# Inject custom CSS to set "Consolas" as the font
+font_css = """
+<style>
+    body {
+        font-family: Consolas, monospace !important;
+    }
+</style>
+"""
+st.markdown(font_css, unsafe_allow_html=True)
+
 st.title('PILOT')
 
 import streamlit as st
@@ -8,7 +19,7 @@ image_path = "chimp.png"
 st.image(image_path, caption="I FIND YOUR LACK OF FAITH DISTURBING.", use_column_width=True)
 
 
-st.write('''
+content = """
     HELLO WORLD
 
     We are currently a team of Technologists. We do the mapping – we do the supply chain. We use cutting edge AI in pursuit of our R&D that aims to push the frontiers of how we perceive intelligent-systems. We are simply devising supply chain and logistics solutions at this point in time. But, we are about to use the same framework that goes into building these solutions to change intelligence as we know it – forever.
@@ -44,4 +55,6 @@ st.write('''
     Check back on day 33 for updated contributions to the chain.
     This is a private chain that will be made public to reasonable contributors – this is to reduce spamming in our project.
      
-''')
+"""
+
+st.write(content)
