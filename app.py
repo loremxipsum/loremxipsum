14a6564ddb8eef34d45919c0bc708cfa46618e2f
loremxipsum <149025169+loremxipsum@users.noreply.github.com>
2023-10-25 21:47:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,18 @@
 import streamlit as st
 
-# Set Streamlit container to wide mode for more space
-st.set_page_config(layout="wide")
-
 font_css = """
 <style>
 @font-face {
     font-family: 'OCRARegular';
-    src: url('ocr-aregular.ttf') format('truetype');
+    src: url('ocr-aregular.ttf') format('truetype');  # Ensure the path to the font file is correct
 }
 .ocr-font {
     font-family: 'OCRARegular';
-    word-wrap: break-word;  /* ensures long strings/words break onto the next line */
 }
 </style>
 """
 
 st.markdown(font_css, unsafe_allow_html=True)
 
-st.title('PILOT')
-
-image_path = "chimp.png" 
-st.image(image_path, caption="I FIND YOUR LACK OF FAITH DISTURBING", use_column_width=True)
-
-# Your content using the OCR font
-content = """
-... [Your content goes here, as previously mentioned]
-"""
-
-st.markdown(f'<div class="ocr-font">{content}</div>', unsafe_allow_html=True)
+# Use the font by wrapping your text in a div with the class 'ocr-font'
+st.markdown('<div class="ocr-font">This is text in OCR A font</div>', unsafe_allow_html=True)
