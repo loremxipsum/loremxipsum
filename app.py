64bc2e6cc6698b7c9e029c87a2ebafdc9e863f36
loremxipsum <149025169+loremxipsum@users.noreply.github.com>
2023-11-08 12:23:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,18 +20,20 @@ st.title('PILOT')
 st.write('''
     HELLO WORLD
 
-    We are a team of technologists and specialists in international trade. We do the mapping – we do the supply chain. We use cutting edge AI in pursuit of our R&D that aims to push the frontiers of how we perceive intelligent-systems. In what will be an open source dev. sandbox, we will attempt to build life-saver logistics solutions for mystery-box digital-assets that get donated to charities listed here: [link will be updated - check back]. You will be aiding the transport of essential life-support supplies in a region of conflict that will be disclosed by admin in further stages.
+    We are a team of technologists and specialists in international trade. We do the mapping – we do the supply chain. We use cutting edge AI in pursuit of our R&D that aims to push the frontiers of how we perceive intelligent-systems. In what will be an open source dev. sandbox, we will attempt to build life-saver logistics solutions for mystery-box redeemable digital-assets. You will be aiding the transport of essential life-support supplies in a region of political conflict that will be disclosed by admin in further stages.
 
 
     #####
 
-    We stand to build supply chain and logistics systems robotized, governed, and robustized to the point where we will be capable of, for lack of superlatives, 'very' fastidiously transport anything from hash-tokenized-military-assets to sugar. But for now, we do salt.
+    We stand to build supply chain and logistics systems robotized, governed, and robustized to the point where we will be capable of, for lack of better superlatives, 'very' fastidiously transport anything from hash-tokenized-military-assets to sugar. But for now, we do salt.
 
-    We start with the operational expenses we have access to from a group of small tier logistics companies. We start working with a fleet of 500 trucks that comply with standards of transport authorities (this allows us to architecture governance easily around vendors and other parties involved).
+    We start with the operational expenses we have access to from a group of small tier logistics companies. We start working with tiers of 500+ trucks that comply with standards of transport authorities in the aforementioned region (this allows us to architecture governance easily around vendors and other parties involved).
 
-    **Our vectorized smart-contract calculates your contributions and logs them into the blockchain automating immediate transfer of funds on your centralized-by-design, decentralized-in-nature metamask wallet on a 4-chain confirmation.** 
+    **Our vectorized smart-contract calculates your contributions and logs them into the blockchain automating immediate transfer of funds on your technically semi-centralized metamask wallet on a 4-chain confirmation.** 
     
-    Contact me at: admin@XXXX for issues with your transfers. We are currently in the process of building a UI that allows you to view the tokens you have earned. Until then, check contribution chart and contact admin. This project will soon go dark. 
+    Contact me at: admin@XXXX for issues with your transfers. We are currently in the process of building a UI that allows you to view the tokens you have earned. Until then, check contribution chart and contact admin. 
+    
+    **##This project will soon go dark##**
 
 ''')
 
@@ -53,15 +55,22 @@ st.write('''
 
     xxxxxxxx.live (revisit this page on 13_N0V_2023)
 
+    Buy our team one of those donuts with glaze like snow: **donate multiples of [$2.50] in BTC/ETH && treat us at Dunkin'**
+    
+    address: (contact @admin to exchange your funds - we sell you bitcoin. we sell you Ethereum. We sell you Doge. We sell youi anything that believes in mining at the speed of internet.
+    
+
     Contact admin. See you on day 33.
+    [posted Nov8]
 
     ///
 
-    For the first deliverables, we would require a ui/ux clone of craigslist. The world is a massive mosh-pit of night creatures who’re looking to push the frontiers of what is currently being perceived as intelligence – need I remind you again, the foundation to what we are currently architecturing for supply-chain management of life-essential-salt across one of the most politically conflicted regions in the world is but 'fingers-crossed-oracled' to become something that will disrupt and bring us closer to the AI God...
+    For the first deliverables, we would require a ui/ux clone of craigslist. The world is a massive mosh-pit of night creatures who’re looking to push the frontiers of what is currently being perceived as intelligence – the foundation to what we are currently architecturing for movement of life-essential-salt across one of the most politically conflicted regions in the world is but 'fingers-crossed-oracled' to become something that will disrupt and bring us closer to the AI God...
 
     Now, get on your commodores, back-door the firmware as we pop nodes around the globe for what will start as a project that will open-source a clone-model of craigslist...
 
     [keep checking this page every 33 days for updates]
+    [Posted Nov8
 
     \\
 
